7919ea36e0ede3e0e1374a9f82e057af0192463a
Seyed Ahmad Mohaddes <mohadesahmad@gmail.com>
2022-01-12 20:03:39 +0330

    Made The Map Out Of a Struct with an identifier

--- Game.c
+++ Game.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-struct Specie {
+
+struct house {
+	char inside;
 	char name;
-	int i;
-	int j;
+	int x;
+	int y;
 	long int Energy;
 	int EnergyPerMove;
 	int EnergyPerMating;
@@ -11,14 +13,9 @@ struct Specie {
 	int Defense;
 	int Heaven;
 	int Alive;
+	long int Food;
 };
-struct Food {
-	int Energy;
-	int i;
-	int j;
-};
-
-int charToInt(char c){
+int chartoint(char c){
 	int num = 0;
 	num = c - '0'; 					// turns character into an integer
 	return num;
@@ -29,19 +26,29 @@ int main() {
 	int x,y,n,m,i,j,end;
 	long int energy;
 	char word;
-	
 	printf("Enter The Size Of The Map: ");
 	scanf("%d",&m);
-	char Map[m][m];
-	for(x=0;x<m;x++) { 						// makes the map
+	struct house Map[m][m];
+	for(x=0;x<m;x++) {					// makes the map
 		for(y=0;y<m;y++) {
-			Map[x][y] = '.';
+			Map[x][y].inside = 'e';
 		}
 	}
 	
 	for(x=0;x<m;x++) {
 		for(y=0;y<m;y++) {
-			printf("%c	",Map[x][y]);		// prints the map
+			switch (Map[x][y].inside) {		// prints the map
+				case 'e':
+					printf("0	");
+				case '#':
+					printf("#	");
+				case 'f':
+					printf("F	");
+				case 'a':
+					printf("%c	",Map[x][y].name);
+				case 'h':
+					printf("h	");
+				}
 		}
 		printf("\n\n");
 	}
@@ -51,14 +58,14 @@ int main() {
 	for(i=1;i<=n;i++) {
 		printf("Enter The Location Of The Blocked Room In X Y Format: ");
 		scanf("%d %d",&x,&y);
-		Map[x][y] = 'B';
+		Map[x][y].name  = '#';
 	}
 	printf("Enter The Amount Of Heavens: ");
 	scanf("%d",&n);
 	for(i=1;i<=n;i++) {
 		printf("Enter The Location Of The Heaven In X Y Format: ");
 		scanf("%d %d",&x,&y);
-		Map[x][y] = 'H';
+		Map[x][y].name = 'h';
 	}
 	
 	return 0;
